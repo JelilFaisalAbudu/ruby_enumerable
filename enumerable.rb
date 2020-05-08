3cb1c67ca66a5efbb71b8539de16ad318bb212c0
JelilFaisalAbudu <abudujelilfaisal@gmail.con>
2020-05-08 16:44:45 +0000

    Add my_each_with_index method to the module

--- enumerable.rb
+++ enumerable.rb
@@ -1,8 +1,7 @@
 module Enumerable
-
   def my_each
     if block_given?
-      self.length.times do |el|
+      length.times do |el|
         yield self[el]
       end
       self
@@ -10,6 +9,17 @@ module Enumerable
       to_enum(:my_each)
     end
   end
+
+  def my_each_with_index
+    if block_given?
+      length.times do |indx|
+        yield self[indx], indx
+      end
+      self
+    else
+      to_enum(:my_each_with_index)
+    end
+  end
 end
 
- p [2, 4, 4].my_each
+p [2, 4, 4].my_each
